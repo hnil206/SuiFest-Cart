6622103604931cd1411336829a9475d6cd7407a2
hnil206 <luund206@gmail.com>
2025-08-18 10:15:54 +0700

    fix: get username by id

--- src/api/auth/nextAuth.ts
+++ src/api/auth/nextAuth.ts
@@ -7,29 +7,42 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.TWITTER_CLIENT_ID!,
       clientSecret: process.env.TWITTER_CLIENT_SECRET!,
       version: '2.0',
+      authorization: {
+        params: {
+          scope: 'tweet.read users.read offline.access',
+        },
+      },
     }),
   ],
   callbacks: {
     async jwt({ token, account, profile }) {
-      console.log('JWT Callback - Account:', account);
-      console.log('JWT Callback - Profile:', profile);
-      console.log('JWT Callback - Token:', token);
-      if (account) {
+      if (account && profile) {
         token.accessToken = account.access_token;
         token.refreshToken = account.refresh_token;
+        
+        // Extract username from Twitter OAuth profile
+        if (profile && 'data' in profile && profile.data) {
+          const profileData = profile.data as any;
+          if (profileData.username) {
+            token.username = profileData.username;
+          }
+        }
+      } else {
+        console.log('Session refresh');
       }
+    
       return token;
     },
     async session({ session, token }) {
-      console.log('Session Callback - Session:', session);
-      console.log('Session Callback - Token:', token);
       session.accessToken = token.accessToken;
       session.refreshToken = token.refreshToken;
       session.twitterId = token.sub;
+      if(token.username){
+        session.username = token.username;
+      }
       return session;
     },
     async redirect({ url, baseUrl }) {
-      console.log('Redirect Callback - URL:', url, 'BaseURL:', baseUrl);
       if (url.startsWith(baseUrl)) return url;
       if (url.startsWith('/')) return `${baseUrl}${url}`;
       return `${baseUrl}/`;
